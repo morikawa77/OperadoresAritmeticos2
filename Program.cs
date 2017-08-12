209bf326ff23113a61b861c61fe5420f10b50c2b
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 03:56:25 -0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperadoresAritmeticos2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int numero;
+			int resto;
+			Console.WriteLine("Digite um número");
+			numero = Convert.ToInt32(Console.ReadLine());
+			resto = numero % 2;
+			Console.WriteLine("O resto da divisão de {0} por 2 é {1}.", numero, resto);
+			Console.ReadLine();
+        }
+    }
+}
